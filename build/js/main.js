3120e26a5439de52e5d6e3f5d9653a8efd7ca521
Seregido665 <sergiotestera666@gmail.com>
2026-02-23 16:00:24 +0100

    Nueva rama y priemra ventana flotante

--- build/js/main.js
+++ build/js/main.js
@@ -10702,3 +10702,27 @@ if ( typeof noGlobal === "undefined" ) {
 
 return jQuery;
 } );
+document.addEventListener('DOMContentLoaded', function () {
+
+  const btnUser = document.querySelector('.right-nav-buttons');
+  const userMenu = document.querySelector('.user-menu-window');
+
+  // PARA QUE SE OCULTE POR DEFECTO
+  userMenu.style.display = 'none';
+
+  // TOOGLE
+  btnUser.addEventListener('click', function (e) {
+    if (userMenu.style.display === 'none') {
+      userMenu.style.display = 'block';
+    } else {
+      userMenu.style.display = 'none';
+    }
+  });
+
+  // OCUALTAR AL HACER click FUERA DEL MENU
+  document.addEventListener('click', function (e) {
+    if (!userMenu.contains(e.target) && !btnUser.contains(e.target)) {
+      userMenu.style.display = 'none';
+    }
+  });
+});
---
+++ src/javascript/custom/main.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  const btnUser = document.querySelector('.right-nav-buttons');
+  const userMenu = document.querySelector('.user-menu-window');
+
+  // PARA QUE SE OCULTE POR DEFECTO
+  userMenu.style.display = 'none';
+
+  // TOOGLE
+  btnUser.addEventListener('click', function (e) {
+    if (userMenu.style.display === 'none') {
+      userMenu.style.display = 'block';
+    } else {
+      userMenu.style.display = 'none';
+    }
+  });
+
+  // OCUALTAR AL HACER click FUERA DEL MENU
+  document.addEventListener('click', function (e) {
+    if (!userMenu.contains(e.target) && !btnUser.contains(e.target)) {
+      userMenu.style.display = 'none';
+    }
+  });
+});
