bb6a9ea545cadcee10b1bde5773709c2e7820e43
Seregido665 <sergiotestera666@gmail.com>
2026-02-24 13:01:43 +0100

    Menu de Mi Perfil ajustado al formato movil

--- build/js/main.js
+++ build/js/main.js
@@ -10706,9 +10706,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const btnUser = document.querySelector('.user-button');
   const userMenu = document.querySelector('.user-menu-window');
+  const btnGlobe = document.querySelector('.globe-button');
+  const globeMenu = document.querySelector('.globe-menu-window');
 
   // PARA QUE SE OCULTE POR DEFECTO
   userMenu.style.display = 'none';
+  globeMenu.style.display = 'none';
 
   // TOOGLE
   btnUser.addEventListener('click', function (e) {
@@ -10719,10 +10722,26 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
+  // TOOGLE
+  btnGlobe.addEventListener('click', function (e) {
+    if (globeMenu.style.display === 'none') {
+      globeMenu.style.display = 'block';
+    } else {
+      globeMenu.style.display = 'none';
+    }
+  });
+
   // OCUALTAR AL HACER click FUERA DEL MENU
   document.addEventListener('click', function (e) {
     if (!userMenu.contains(e.target) && !btnUser.contains(e.target)) {
       userMenu.style.display = 'none';
+    }
+  });
+
+  // OCUALTAR AL HACER click FUERA DEL MENU
+  document.addEventListener('click', function (e) {
+    if (!globeMenu.contains(e.target) && !btnGlobe.contains(e.target)) {
+      globeMenu.style.display = 'none';
     }
   });
 });
