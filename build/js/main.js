4a79127f14c4baeae0ef4062eecccdac94f1a647
Seregido665 <sergiotestera666@gmail.com>
2026-02-24 20:49:39 +0100

    Menus de navbar adapatamos a la vista movil

--- build/js/main.js
+++ build/js/main.js
@@ -10710,6 +10710,12 @@ document.addEventListener('DOMContentLoaded', function () {
   const globeMenu = document.querySelector('.globe-menu-window');
   const btnBurguer = document.querySelector('.burguer-button');
   const burguerMenu = document.querySelector('.burguer-menu-window');
+  // HACE REFERENCIA PRIMERO AL BOTON PADRE(.burguer-menu-window) PARA PODER APUNTAR 
+  // LUEGO AL BOTON HIJO(.globe-button).
+  const btnGlobeInBurguer = document.querySelector('.burguer-menu-window > .globe-button'); 
+  // HACIENDO REFERENCIA PRIMERO AL PADRE SE PUEDE LUEGO BUSCAR A .globe-menu-window SIN IMPORTAR 
+  // SU NIVEL DE ANIDACION DENTRO DE .burguer-menu-window
+  const globeMenuInBurguer = document.querySelector('.burguer-menu-window .globe-menu-window');
   const navRight = document.querySelector('.right-nav-buttons');         
   const btnClose = document.querySelector('.right-nav-buttons .close');  
   const mobileQuery = window.matchMedia('(max-width: 30rem)');   // PARA DETECTAR SI ESTAMOS EN TAMAÑO MÓVIL
@@ -10718,6 +10724,7 @@ document.addEventListener('DOMContentLoaded', function () {
   userMenu.style.display = 'none';
   globeMenu.style.display = 'none';
   burguerMenu.style.display = 'none';
+  globeMenuInBurguer.style.display = 'none';
 
   // FUNCIONES PARA TRAER LAS CLASES DE nav-buttons-right.scss
   const menuUser = function (isOpen) {
@@ -10750,6 +10757,14 @@ document.addEventListener('DOMContentLoaded', function () {
       globeMenu.style.display = 'none';
     }
   });
+  // -- Idioma (en Menú Burguer) --
+  btnGlobeInBurguer.addEventListener('click', function (e) {
+    if (globeMenuInBurguer.style.display === 'none') {
+      globeMenuInBurguer.style.display = 'block';
+    } else {
+      globeMenuInBurguer.style.display = 'none';
+    }
+  });
 
   // -- Menú Burguer --
   btnBurguer.addEventListener('click', function (e) {
@@ -10777,6 +10792,12 @@ document.addEventListener('DOMContentLoaded', function () {
   document.addEventListener('click', function (e) {
     if (!mobileQuery.matches && !globeMenu.contains(e.target) && !btnGlobe.contains(e.target)) {
       globeMenu.style.display = 'none';
+    }
+  });
+  // Idioma (en Menú Burguer)
+  document.addEventListener('click', function (e) {
+    if (btnGlobeInBurguer && globeMenuInBurguer && !globeMenuInBurguer.contains(e.target) && !btnGlobeInBurguer.contains(e.target)) {
+      globeMenuInBurguer.style.display = 'none';
     }
   });
   // Menú Burguer
