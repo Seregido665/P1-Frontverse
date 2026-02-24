b7dfa331971edbf75f39154af18870318664e625
Seregido665 <sergiotestera666@gmail.com>
2026-02-24 14:07:00 +0100

    Detalles

--- src/javascript/custom/main.js
+++ src/javascript/custom/main.js
@@ -4,21 +4,34 @@ document.addEventListener('DOMContentLoaded', function () {
   const userMenu = document.querySelector('.user-menu-window');
   const btnGlobe = document.querySelector('.globe-button');
   const globeMenu = document.querySelector('.globe-menu-window');
-
-  // PARA QUE SE OCULTE POR DEFECTO
+  const navRight = document.querySelector('.right-nav-buttons');         
+  const btnClose = document.querySelector('.right-nav-buttons .close');  
+  const mobileQuery = window.matchMedia('(max-width: 30rem)');   // PARA DETECTAR SI ESTAMOS EN TAMAÑO MÓVIL
+  
+  // PARA QUE LOS MENUS ESTEN OCULTOS POR DEFECTO
   userMenu.style.display = 'none';
   globeMenu.style.display = 'none';
 
-  // TOOGLE
+
+  // - TOGGLE DE Mi Perfil -
+  const menuUser = function (isOpen) {
+    navRight.classList.toggle('mobile-user-open', isOpen);
+  };
   btnUser.addEventListener('click', function (e) {
+
+    // - PARA LA VISTA DE ESCRITORIO -
     if (userMenu.style.display === 'none') {
       userMenu.style.display = 'block';
     } else {
       userMenu.style.display = 'none';
     }
+    // - PARA LA VISTA DE MOVIL -
+    if (mobileQuery.matches) {
+      menuUser(userMenu.style.display === 'block');
+    }
   });
 
-  // TOOGLE
+  // - TOGGLE DE Idioma -
   btnGlobe.addEventListener('click', function (e) {
     if (globeMenu.style.display === 'none') {
       globeMenu.style.display = 'block';
@@ -27,17 +40,25 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-  // OCUALTAR AL HACER click FUERA DEL MENU
+
+  // OCUALTAR AL HACER click FUERA
   document.addEventListener('click', function (e) {
-    if (!userMenu.contains(e.target) && !btnUser.contains(e.target)) {
+    if (!mobileQuery.matches && !userMenu.contains(e.target) && !btnUser.contains(e.target)) {
       userMenu.style.display = 'none';
     }
   });
-
-  // OCUALTAR AL HACER click FUERA DEL MENU
+  // OCUALTAR AL HACER click FUERA
   document.addEventListener('click', function (e) {
-    if (!globeMenu.contains(e.target) && !btnGlobe.contains(e.target)) {
+    if (!mobileQuery.matches && !globeMenu.contains(e.target) && !btnGlobe.contains(e.target)) {
       globeMenu.style.display = 'none';
+    }
+  });
+
+  // -- FUNCION PARA EL BOTON DE CERRAR --
+  btnClose.addEventListener('click', function (e) {
+    if (mobileQuery.matches) {
+      userMenu.style.display = 'none';
+      menuUser(false);
     }
   });
 });
